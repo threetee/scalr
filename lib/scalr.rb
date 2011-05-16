760057d0db0218b00284b1795547a0c40da231ae
John Schult <schulty@vitrue.com>
2011-05-16 11:00:22 -0400

    [CHANGE] Works with current versions of ActiveSupport

--- lib/scalr.rb
+++ lib/scalr.rb
@@ -1,5 +1,5 @@
 require 'rubygems'
-require 'active_support'
+require 'active_support/core_ext'
 
 require File.dirname(__FILE__) + '/scalr/response'
 require File.dirname(__FILE__) + '/scalr/request'
