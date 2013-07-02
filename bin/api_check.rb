45be82d0bf4eaefe47b2335b77ce8c940ec9784f
Chris Winters <chris@cwinters.com>
2013-07-02 16:41:32 -0400

    add "match" action to display API calls matching a substring

--- bin/api_check.rb
+++ bin/api_check.rb
@@ -7,15 +7,13 @@ def actions
   Scalr::Request::ACTIONS
 end
 
-def all
+def all(args)
   actions.keys.sort.each do |ruby_name|
-    config = actions[ruby_name]
-    puts show_name_and_version(ruby_name, config)
-    puts show_inputs(config).join("\n"), "\n"
+    show(ruby_name, actions[ruby_name])
   end  
 end
 
-def check
+def check(args)
   failures = 0
   actions.keys.sort.each do |ruby_name|
     config = actions[ruby_name]
@@ -32,20 +30,32 @@ def check
   puts "EVERYTHING OK!!!!!"  if failures == 0
 end
 
-def help
+def help(args)
   puts <<-HELP
   #{$0} - Check/display Scalr API configuration
 
-  Usage: #{$0} [action - defaults to 'check']
+  Usage: #{$0} [action - defaults to 'check'] [arg...]
   
   Actions:
     all:   list all API calls and arguments
     check: display any API calls that are misconfigured
     help:  show this message
+    match: list API calls and arguments where the call contains a string
   HELP
 end
 
+def match(args)
+  return all(args) if args.empty?
+  substring = args[0]
+  actions.keys.sort.each do |ruby_name|
+    show(ruby_name, actions[ruby_name]) if ruby_name.to_s.include?(substring)    
+  end
+end
 
+def show(ruby_name, config)
+  puts show_name_and_version(ruby_name, config)
+  puts show_inputs(config).join("\n"), "\n"
+end
 
 
 def find_issues(api_config)
@@ -65,13 +75,14 @@ def input_key_exists?(key)
 end
 
 def main(args)
-  action = args.empty? ? 'check' : args[0]
-  self.send(action.to_sym)
+  action = args.empty? ? 'check' : args.shift
+  self.send(action.to_sym, args)
 end
 
 def show_inputs(config)  
   config[:inputs].keys.sort.map do |ruby_key|
-    "  => #{ruby_key} (#{input_key(ruby_key)}) [Required? #{config[:inputs][ruby_key].to_s}]"
+    default_display = config[:defaults] && config[:defaults][ruby_key] ? " => default: #{config[:defaults][ruby_key]}" : ''
+    "  => #{ruby_key} (#{input_key(ruby_key)}) [Required? #{config[:inputs][ruby_key].to_s}#{default_display}]"
   end
 end
 
