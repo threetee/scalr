d0cc969eed1969beba53bb334f6400d96a400db9
Chris Geihsler <cgeihsler@thinkthroughmath.com>
2013-07-23 13:53:01 -0400

    Added rails_console command

---
+++ lib/scalr/ssher.rb
@@ -0,0 +1,49 @@
+module Scalr
+  class Ssher
+    attr_reader :params
+
+    def initialize(params)
+      @params  = params
+    end
+
+    def execute(remote_command = '')
+      response = dispatch(:farm_get_details, params)
+      return if generic_error(response)
+
+      server = Scalr::ResponseObject::FarmRole.
+                 single_server(response.content, params['server'].value)
+
+      unless server
+        $stderr.puts("Failed to identify server with: #{params['server'].value}")
+        exit_status 1
+        return
+      end
+
+      farm_id = response.request_inputs['FarmID']
+      key_path = File.expand_path("~/.ssh/FARM-#{farm_id}.#{server.platform_properties.availability_zone_brief}.private.pem")
+      if File.exists?(key_path)
+        command = "ssh -i #{key_path} root@#{server.external_ip} #{remote_command}"
+        puts "Executing `#{command}`"
+        exec command
+      else
+        $stderr.puts(<<-KEYFILE.gsub(/^ {10}/, ''))
+
+          Expected key file (#{key_path}) does not exist.
+          Here's how to fix it:
+            - go to https://my.scalr.com/#/sshkeys/view
+            - find the row with the 'Farm ID' column as #{farm_id}
+            - click the 'Actions' dropdown in its far right column
+            - choose 'Download private key'
+            - store it to #{key_path}
+            - execute: 'chmod 400 #{key_path}'
+              (so ssh won't complain about permissive permissions)
+        KEYFILE
+        exit_status 1
+      end
+    end
+
+    def dispatch(action_name, params)
+      Scalr::Caller.new(action_name).dispatch(params)
+    end
+  end
+end
