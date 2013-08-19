6fbc2a0cbec5df286185ae9920a781ab7d1a48d1
Chris Winters <chris@cwinters.com>
2013-08-19 15:07:41 -0400

    add "ttmscalr scp" to easily copy files from a scalr server to your local machine

--- lib/scalr/ssher.rb
+++ lib/scalr/ssher.rb
@@ -1,32 +1,43 @@
 module Scalr
   class Ssher
-    attr_reader :params
+    attr_reader :exit_status, :farm_id, :key_path, :params, :server
 
     def initialize(params)
       @params  = params
+      @exit_status = 0
     end
 
     def execute(remote_command = '')
-      response = dispatch(:farm_get_details, params)
-      return if generic_error(response)
+      return unless identify_server
+      return unless check_key_path
 
-      server = Scalr::ResponseObject::FarmRole.
-                 single_server(response.content, params['server'].value)
+      cmd = params.has_key?('cmd') ? params['cmd'].value : 'ssh'
+      command = "#{cmd} -i #{key_path} root@#{server.external_ip} #{remote_command}"
+      puts "Executing `#{command}`"
+      exec command
+    end
 
-      unless server
-        $stderr.puts("Failed to identify server with: #{params['server'].value}")
-        exit_status 1
+    def execute_scp(download_path, local_path)
+      unless download_path && local_path
+        $stderr.puts("Cannot SCP: both download path (#{download_path}) and local path (#{local_path}) must be defined.")
+        @exit_status = 1
         return
       end
 
-      farm_id = response.request_inputs['FarmID']
-      key_path = File.expand_path("~/.ssh/FARM-#{farm_id}.#{server.platform_properties.availability_zone_brief}.private.pem")
-      if File.exists?(key_path)
-        cmd = ( params.has_key?('cmd')? params['cmd'].value : "ssh" )
-        command = "#{cmd} -i #{key_path} root@#{server.external_ip} #{remote_command}"
-        puts "Executing `#{command}`"
-        exec command
-      else
+      return unless identify_server
+      return unless check_key_path
+
+      command = "scp -i #{key_path} root@#{server.external_ip}:#{download_path} #{local_path}"
+      puts "Executing `#{command}`"
+      exec command
+    end
+
+  private
+    def check_key_path
+      return @key_path if @key_path
+
+      @key_path = File.expand_path("~/.ssh/FARM-#{farm_id}.#{server.platform_properties.availability_zone_brief}.private.pem")
+      unless File.exists?(key_path)
         $stderr.puts(<<-KEYFILE.gsub(/^ {10}/, ''))
 
           Expected key file (#{key_path}) does not exist.
@@ -39,12 +50,30 @@ module Scalr
             - execute: 'chmod 400 #{key_path}'
               (so ssh won't complain about permissive permissions)
         KEYFILE
-        exit_status 1
+        @exit_status = 1
       end
+      @key_path
     end
 
     def dispatch(action_name, params)
       Scalr::Caller.new(action_name).dispatch(params)
+    end
+
+    def identify_server
+      return @server if @server
+
+      response = dispatch(:farm_get_details, params)
+      return if generic_error(response)
+
+      @server = Scalr::ResponseObject::FarmRole.
+                  single_server(response.content, params['server'].value)
+      if @server
+        @farm_id = response.request_inputs['FarmID']
+      else
+        $stderr.puts("Failed to identify server with: #{params['server'].value}")
+        @exit_status = 1
+      end
+      @server
     end
   end
 end
