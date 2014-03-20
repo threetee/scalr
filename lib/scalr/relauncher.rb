677af67dadcdf2d75e9837a6beb94bb327a4a159
Andre Henry <ahenry@thinkthroughmath.com>
2014-03-20 15:44:05 -0400

    monitor a server from boot to running

--- lib/scalr/relauncher.rb
+++ lib/scalr/relauncher.rb
@@ -4,12 +4,23 @@ require 'pry'
 
 class ReplacementServer
   attr_accessor :status
-  attr_reader :server_id, :target
+  attr_reader :server_id, :target, :name
 
-  def initialize(target, server_id)
+  def initialize(target, server_id, name)
     @target = target
     @server_id = server_id
-    @status = :pending
+    @status = :Pending
+    @name = name
+  end
+end
+
+class ReplacementStatus
+  attr_accessor :running, :booting
+
+  def initialize(role, running, booting)
+    @role = role
+    @running = running
+    @booting = booting
   end
 end
 
@@ -22,35 +33,56 @@ class Relauncher
   end
 
   def launch
-    @roles.each { |role|
+    @roles.each do |role|
       launched_servers = []
-      role.servers.each { |server|
+      role.servers.each do |server|
         response = perform_launch({:farm_id => @farm_id, :farm_role_id => role.id})
-        launched_servers << ReplacementServer.new(server.id, response.content)
+        name = sprintf("%s-%s", role.name, server.index)
+        launched_servers << ReplacementServer.new(server.id, response.content, name)
         break
-      }
+      end
       @replacements.store(role.id, launched_servers)
       break
-    }
+    end
     puts "Launched - #{@replacements}"
   end
 
-  def monitor
-    farm_status = get_farm_status
+  def update_role_status
 
+  end
+
+  def monitor
+    pending = true
+
+    while pending
+      pending = false
+      @replacements.each do |role, servers|
+        pending_servers = servers.select { |s| s.status != :Running }
+        if pending_servers.length > 0
+          update_launch_status
+          pending = true
+          next
+        end
+      end
+    end
+  end
+
+  private
+
+  def update_launch_status
+    farm_status = get_farm_status
     farm_status.each { |role|
       servers = role.servers
       replacement_servers = @replacements[role.id]
       if replacement_servers
         replacement_servers.each { |replacement|
           server = servers.select { |x| x.id == replacement.server_id }
-          puts "#{replacement.target} -> #{server[0].id} -> #{server[0].status}"
+          replacement.status = server[0].status.to_sym
+          puts "#{replacement.target} - #{replacement.name} -> #{server[0].id} -> #{server[0].status}"
         }
       end
     }
   end
-
-  private
 
   def perform_launch(options)
     invoke(:server_launch, options)
